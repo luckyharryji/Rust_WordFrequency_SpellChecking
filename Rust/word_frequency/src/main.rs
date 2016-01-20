2254253ea611fa396ea66f195f262f394c012123
luckyharryji <xiangyu.ji@outlook.com>
2016-01-20 09:58:52 -0800

    finish assign 1

--- Rust/word_frequency/src/main.rs
+++ Rust/word_frequency/src/main.rs
@@ -1,7 +1,42 @@
 #[doc="
-Counts the frequencies of words read from the standard input, and print
+author = '__Xiangyu__'
+
+Word Frequency
+
+Counts the frequencies of words read from the file, file name got through standard input, and print
 a sorted frequency table.
+
+INPUT:
+
+The input format is the file name, one execution perline
+
+Each time give an input, the program count and print the word frequency inside the input file.
+
+The program terminates with the input of EOF
+
+OUTPUT:
+
+The program counts the word frequency inside the input file, the number is considered invalid and will
+be considerd the space between the words. It prints the results in this format:
+
+    world: 2
+    bye: 1
+    test: 1
+    hello: 1
+    wor: 1
+    ld: 1
+
 Assumptions:
+
+  - Numeriacal numbers are considered as the space between words, is means if meet a string like 'wor3ld',
+  the program will consider it as 2 words 'wor' and 'ld'.
+
+  - Input does not need to be perfect, the programs controls through io::File.
+
+  - If there is nothing in the file, program would start a new line to wait for the new coming input.
+
+  - The termination of the program is the Input with the string text 'EOF', not a line/word inside text that 
+  is 'EOF'.
 "]
 
 extern crate itertools;
@@ -24,11 +59,11 @@ fn get_input_file_name<R: Read>(reader: R){
 
     while let Some(Ok(line)) = lines.next() {
         if line == "EOF" {break}
-        read_and_count_and_print(line);
+        print_word_frequency_of_file(line);
     }
 }
 
-fn read_and_count_and_print(file_name:String)-> std::io::Result<()>{
+fn print_word_frequency_of_file(file_name:String)-> std::io::Result<()>{
     let f = try!(File::open(file_name));
     let mut reader = BufReader::new(f).lines();
     let mut count_table = CountTable::new();
@@ -64,100 +99,30 @@ fn sort_by_freq(count_table: CountTable) -> String {
         _ => count_table.iter()
                 .sort_by(|a, b| Ord::cmp(&b.1, &a.1))
                 .iter()
-                .map(|&(key, value)| format!("{} {}", key, value))
+                .map(|&(key, value)| format!("{}: {}", key, value))
                 .fold("".to_string(), |i, v| format!("{}{}\n", i, v))
     }
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// #[cfg(test)]
-// mod increment_word_tests {
-//     use super::{increment_word, CountTable};
-
-//     #[test]
-//     fn test_case_initial(){
-//         assert_eq!(1, 1);
-//     }
-
-//     #[test]
-//     fn inserts_if_empty() {
-//         let mut h = CountTable::new();
-//         increment_word(&mut h, "one".to_owned());
-
-//         assert_eq!(Some(&1), h.get("one"));
-//         assert_eq!(1, h.len());
-//     }
-
-//     #[test]
-//     fn increments_if_present() {
-//         let mut under_test = fixture();
-//         let mut expected   = fixture();
-
-//         increment_word(&mut under_test, "three".to_owned());
-//         expected.insert("three".to_owned(), 4);
-
-//         assert_eq!(expected, under_test);
-//     }
-
-//     #[test]
-//     fn insert_if_absent() {
-//         let mut under_test = fixture();
-//         let mut expected   = fixture();
-
-//         increment_word(&mut under_test, "one".to_owned());
-//         expected.insert("one".to_owned(), 1);
-
-//         assert_eq!(expected, under_test);
-//     }
-
-//     fn fixture() -> CountTable {
-//         let mut h = CountTable::new();
-//         h.insert("two".to_owned(), 2);
-//         h.insert("three".to_owned(), 3);
-
-//         assert_eq!(None, h.get("one"));
-//         assert_eq!(Some(&2), h.get("two"));
-//         assert_eq!(Some(&3), h.get("three"));
-//         assert_eq!(2, h.len());
-//         h
-//     }
-// }
-
 #[cfg(test)]
 mod sort_count_table_tests {
-    use super::{sort_by_freq, increment_word, CountTable};
+    use super::{sort_by_freq, CountTable};
 
     #[test]
     fn change_line_if_empty() {
-        let mut h = CountTable::new();
+        let h = CountTable::new();
         let sort_table_string = sort_by_freq(h);
 
         assert_eq!("\n", sort_table_string);
     }
 
     #[test]
-    fn sort_if_valid(){
-        let mut h = CountTable::new();
-        h.insert("one".to_owned(),1);
-        h.insert("tow".to_owned(),2);
-
+    fn sort_descending_order_if_valid(){
+        let h = table_initial();
         let sort_table_string = sort_by_freq(h);
-        assert_eq!("tow 2\none 1\n", sort_table_string);
+        
+        assert_eq!("three: 3\ntwo: 2\n", sort_table_string);
     }
 
     fn table_initial() -> CountTable {
